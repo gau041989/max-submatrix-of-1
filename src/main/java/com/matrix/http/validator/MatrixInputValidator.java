15a89053b611d33e2903bee48c0a4a9c2d0598d0
Gaurav Pasarkar <gpasarkar@equalexperts.com>
2019-05-13 23:34:25 +0530

    Create separate predicates for individual filters

--- src/main/java/com/matrix/http/validator/MatrixInputValidator.java
+++ src/main/java/com/matrix/http/validator/MatrixInputValidator.java
@@ -5,20 +5,31 @@ import com.matrix.http.model.InvalidMatrixException;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
 
 @Component
 public class MatrixInputValidator {
 
+    private static Predicate<Input> isMatrixNonEmpty = (input -> input.getMatrix().length == 0);
+
+    private static Predicate<Input> doesMatrixDimensionMatchColumnLength = (input -> input.getMatrix().length != input.getColLength());
+
+    private static Predicate<Input> doesMatrixHaveEqualRowLength = (input ->
+        (input.getMatrix().length > 0)
+                &&
+        (Arrays.stream(input.getMatrix()).anyMatch(arr -> arr.length != input.getRowLength()))
+    );
+
+    private static List<Predicate<Input>> ALL_FILTERS = Arrays.asList(
+            isMatrixNonEmpty, doesMatrixDimensionMatchColumnLength, doesMatrixHaveEqualRowLength);
+
     public Input validate(Input input) throws InvalidMatrixException {
-        if(input.getMatrix().length == 0 || input.getMatrix().length != input.getColLength()) {
+
+        if(ALL_FILTERS.stream().anyMatch(predicate -> predicate.test(input))) {
             throw new InvalidMatrixException();
         }
 
-        if(input.getMatrix().length > 0) {
-            if(Arrays.stream(input.getMatrix()).anyMatch(arr -> arr.length != input.getRowLength())) {
-                throw new InvalidMatrixException();
-            }
-        }
         return input;
     }
 }
